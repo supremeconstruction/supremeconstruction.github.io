3dd069b34118851c4b311eb8af0b4f6b49d03dc6
supremeconstruction <supreme_construction@verizon.net>
2016-04-11 12:40:36 -0700

    form
    form

---
+++ p4/js/index.js
@@ -0,0 +1,5 @@
+var mapCanvas = document.getElementById("map");
+var mapOptions = {
+    center: new google.maps.LatLng(34.055979, -118.424464), zoom: 30
+}
+var map = new google.maps.Map(mapCanvas, mapOptions);
